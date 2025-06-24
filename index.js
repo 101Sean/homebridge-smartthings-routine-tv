643c88c205a0a2bb5a873a86b131733e801b23c7
mikeryu98 <mikeryu98@gmail.com>
2025-06-25 05:12:52 +0900

    v1.0.5

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // index.js
 const axios = require('axios');
 const pkg   = require('./package.json');
-const pluginName = pkg.name;  // "homebridge-smartthings-routine-tv"
+const pluginName = pkg.name;           // "homebridge-smartthings-routine-tv"
 
 let Service, Characteristic, uuid;
 
-module.exports = (api) => {
+module.exports = api => {
     Service        = api.hap.Service;
     Characteristic = api.hap.Characteristic;
     uuid           = api.hap.uuid;
 
-    // Accessory 플러그인 등록
+    // Accessory 방식으로 등록
     api.registerAccessory(pluginName, 'StRoutineTV', StRoutineTV);
 };
 
@@ -22,16 +22,16 @@ class StRoutineTV {
         this.routineId = config.routineId;
 
         if (!this.name || !this.token || !this.routineId) {
-            throw new Error('config.json에 name, token, routineId가 모두 필요합니다');
+            throw new Error('config.json에 name, token, routineId 모두 필요합니다');
         }
 
-        // AccessoryInformation 서비스 (TV 아이콘 강제 지정)
-        this.informationService = new Service.AccessoryInformation()
+        // 1) AccessoryInformation (아이콘 강제 지정)
+        this.infoService = new Service.AccessoryInformation()
             .setCharacteristic(Characteristic.Manufacturer, 'Custom')
             .setCharacteristic(Characteristic.Model,        'Television')
             .setCharacteristic(Characteristic.Name,         this.name);
 
-        // Television 서비스 구성 (필수 7요소)
+        // 2) Television 서비스 구성 (필수 7요소)
         this.tvService = new Service.Television(this.name)
             .setCharacteristic(Characteristic.ConfiguredName, this.name)
             .setCharacteristic(
@@ -48,16 +48,16 @@ class StRoutineTV {
         this.tvService.getCharacteristic(Characteristic.RemoteKey)
             .onSet((_, cb) => cb());
 
-        // Dummy InputSource
+        // InputSource 더미
         const input = new Service.InputSource(
             `${this.name} Input`,
             uuid.generate(this.routineId + '-in')
         );
         input
-            .setCharacteristic(Characteristic.Identifier,              1)
-            .setCharacteristic(Characteristic.ConfiguredName,          this.name)
-            .setCharacteristic(Characteristic.IsConfigured,            Characteristic.IsConfigured.CONFIGURED)
-            .setCharacteristic(Characteristic.InputSourceType,         Characteristic.InputSourceType.HDMI)
+            .setCharacteristic(Characteristic.Identifier,             1)
+            .setCharacteristic(Characteristic.ConfiguredName,         this.name)
+            .setCharacteristic(Characteristic.IsConfigured,           Characteristic.IsConfigured.CONFIGURED)
+            .setCharacteristic(Characteristic.InputSourceType,        Characteristic.InputSourceType.HDMI)
             .setCharacteristic(
                 Characteristic.CurrentVisibilityState,
                 Characteristic.CurrentVisibilityState.SHOWN
@@ -68,7 +68,7 @@ class StRoutineTV {
         // Active 토글 (원터치 복원)
         this.tvService.getCharacteristic(Characteristic.Active)
             .onGet(() => Characteristic.Active.INACTIVE)
-            .onSet(async (value) => {
+            .onSet(async value => {
                 if (value === Characteristic.Active.ACTIVE) {
                     try {
                         await axios.post(
@@ -77,13 +77,12 @@ class StRoutineTV {
                             { headers: { Authorization: `Bearer ${this.token}` } }
                         );
                         this.log.info(`Executed TV routine: ${this.name}`);
-                    } catch (err) {
-                        this.log.error('Error executing TV routine', err);
+                    } catch (e) {
+                        this.log.error('Error executing TV routine', e);
                         throw new this.api.hap.HapStatusError(
                             this.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
                         );
                     } finally {
-                        // 원터치 상태 복원
                         this.tvService.updateCharacteristic(
                             Characteristic.Active,
                             Characteristic.Active.INACTIVE
@@ -93,8 +92,8 @@ class StRoutineTV {
             });
     }
 
-    // Homebridge 가 호출: 이 배열의 서비스들이 HAP 액세서리로 노출됩니다
+    // Homebridge가 이 메서드를 호출해서 HAP 액세서리로 노출합니다
     getServices() {
-        return [ this.informationService, this.tvService ];
+        return [ this.infoService, this.tvService ];
     }
 }
